3ed08069a68f78a2961d5475bd0a2810963d95e1
demono10000 <pawel@soltys.com.pl>
2022-10-20 09:23:54 +0200

    pierwsze laby

---
+++ Pierwszy/Pierwszy/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Pierwszy.services;
+
+namespace Pierwszy.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var calculateInstance = new Calculate(23);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet]
+        public List<int> GetValue()
+        {
+            var calculateInstance = new Calculate(23);
+            calculateInstance.Add(10);
+            calculateInstance.Decrease(5);
+            var calculateInstance2 = new Calculate(40);
+            return new List<int>() { calculateInstance.GetValue(), calculateInstance2.GetValue() };
+        }
+    }
+}
---
+++ Pierwszy/Pierwszy/services/Calculate.cs
@@ -0,0 +1,24 @@
+﻿namespace Pierwszy.services
+{
+    public class Calculate
+    {
+        public Calculate(int initVal)
+        {
+            Value = initVal;
+        }
+        private int Value { get; set; }
+
+        public void Add(int value)
+        {
+            Value += value;
+        }
+        public void Decrease(int value)
+        {
+            Value -= value;
+        }
+        public int GetValue()
+        {
+            return Value;
+        }
+    }
+}
