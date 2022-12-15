7faa24afee2a2cf73fb9dfa8d1d35e8e2a45a97f
demono10000 <pawel@soltys.com.pl>
2022-12-15 09:53:49 +0100

    zaległe zmiany

---
+++ WebApplication1/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Person;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new PersonViewModel()
+            {
+                Persons = new PersonModel().GetPersons()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Person/GenderEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models.Person
+{
+    public enum GenderEnum
+    {
+        Male = 1,
+        Female = 2
+    }
+}
